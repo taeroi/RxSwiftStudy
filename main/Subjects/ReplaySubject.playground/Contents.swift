bdb1868f4622d587aea02ce93f1bbe61877cf45b
taeroi <fkrkdlsgk@gmail.com>
2021-04-15 10:51:23 +0900

    feat: Study ReplaySubjects

--- main/Subjects/ReplaySubject.playground/Contents.swift
+++ main/Subjects/ReplaySubject.playground/Contents.swift
@@ -34,4 +34,28 @@ enum MyError: Error {
    case error
 }
 
+//이전 Subjects와는 다르게 create로 생성한다 (버퍼사이즈 생성)
+let rs = ReplaySubject<Int>.create(bufferSize: 3)
 
+(1...10).forEach {rs.onNext($0)}
+
+rs.subscribe {print("Observer 1>>", $0)}
+    .disposed(by: disposeBag)
+// 출력결과는 버퍼에 마지막에 전달된 3개가 저장되므로 next 이벤트에는 8,9,10이 저장됨
+
+rs.subscribe {print("Observer 2>>",$0)}
+    .disposed(by: disposeBag)
+
+rs.onNext(11)
+
+//새로운 구독자 생성
+rs.subscribe {print("Observer 3>>",$0)}
+    .disposed(by: disposeBag)
+// 출력결과는 next 이벤트로 받은것 + 버퍼에 마지막에 전달된 것
+
+// completed,error 이벤트는 새로운 구독자의 이벤트 실행 후 종료됨
+rs.onCompleted()
+
+
+rs.subscribe {print("Observer 4>>",$0)}
+    .disposed(by: disposeBag)
