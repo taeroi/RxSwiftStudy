d12f29bf94717392c239fefb6a4119238976cd1f
taeroi <fkrkdlsgk@gmail.com>
2021-04-21 11:44:59 +0900

    study: Gesture control - view handle

--- main/MasteringRxSwift/RxCocoaCommonPatterns/Gesture/RxCocoaGestureViewController.swift
+++ main/MasteringRxSwift/RxCocoaCommonPatterns/Gesture/RxCocoaGestureViewController.swift
@@ -25,18 +25,30 @@ import RxSwift
 import RxCocoa
 
 class RxCocoaGestureViewController: UIViewController {
-   
-   let bag = DisposeBag()
-   
-   @IBOutlet weak var targetView: UIView!
-   
-   @IBOutlet var panGesture: UIPanGestureRecognizer!
-   
-   override func viewDidLoad() {
-      super.viewDidLoad()
-      
-      targetView.center = view.center
-      
-      
-   }   
+    
+    let bag = DisposeBag()
+    
+    @IBOutlet weak var targetView: UIView!
+    
+    @IBOutlet var panGesture: UIPanGestureRecognizer!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        targetView.center = view.center
+        
+        panGesture.rx.event
+            .subscribe(onNext: {[unowned self] gesture in
+                guard let target = gesture.view else {return}
+                
+                let translation = gesture.translation(in: self.view)
+                target.center.x += translation.x
+                target.center.y += translation.y
+                
+                gesture.setTranslation(.zero, in: self.view)
+            })
+            .disposed(by: bag)
+        
+        
+    }
 }
