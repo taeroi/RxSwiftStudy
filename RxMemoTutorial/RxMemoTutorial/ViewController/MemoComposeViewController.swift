442c5d945aac687d35684edcc5dcdc629fb1b016
taeroi <fkrkdlsgk@gmail.com>
2021-04-22 17:32:12 +0900

    feat: Add ComposeViewController

--- RxMemoTutorial/RxMemoTutorial/ViewController/MemoComposeViewController.swift
+++ RxMemoTutorial/RxMemoTutorial/ViewController/MemoComposeViewController.swift
@@ -6,19 +6,55 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+import Action
+import NSObject_Rx
 
 class MemoComposeViewController: UIViewController, ViewModelBindableType {
-
+    
     var viewModel: MemoComposeViewModel!
-
+    
+    @IBOutlet weak var cancelButton: UIBarButtonItem!
+    @IBOutlet weak var saveButton: UIBarButtonItem!
+    @IBOutlet weak var contentTextView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         
     }
     
     func bindViewModel() {
+        viewModel.title
+            .drive(navigationItem.rx.title)
+            .disposed(by: rx_disposeBag)
+        
+        viewModel.initialText
+            .drive(contentTextView.rx.text)
+            .disposed(by: rx_disposeBag)
+        
+        cancelButton.rx.action = viewModel.cancelAction
+        
+        //더블 탭을 막기 위하여 throttle 사용
+        saveButton.rx.tap
+            .throttle(.milliseconds(500), scheduler: MainScheduler.instance)
+            .withLatestFrom(contentTextView.rx.text.orEmpty)
+            .bind(to: viewModel.saveAction.inputs)
+            .disposed(by: rx_disposeBag)
         
     }
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        contentTextView.becomeFirstResponder()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        if contentTextView.isFirstResponder {
+            contentTextView.resignFirstResponder()
+        }
+    }
+    
 }
