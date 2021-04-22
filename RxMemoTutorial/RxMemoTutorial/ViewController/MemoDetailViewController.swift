d9905cc5eb2f0a1410def38ac73aee8a4e4f4202
taeroi <fkrkdlsgk@gmail.com>
2021-04-22 18:33:07 +0900

    feat: Add DetailViewController
    descript bindViewModel in Detail VC

--- RxMemoTutorial/RxMemoTutorial/ViewController/MemoDetailViewController.swift
+++ RxMemoTutorial/RxMemoTutorial/ViewController/MemoDetailViewController.swift
@@ -8,9 +8,9 @@
 import UIKit
 
 class MemoDetailViewController: UIViewController, ViewModelBindableType {
-
+    
     var viewModel: MemoDetailViewModel!
-
+    
     @IBOutlet weak var listTableView: UITableView!
     @IBOutlet weak var deleteButton: UIBarButtonItem!
     @IBOutlet weak var editButton: UIBarButtonItem!
@@ -18,12 +18,32 @@ class MemoDetailViewController: UIViewController, ViewModelBindableType {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         
     }
     
     func bindViewModel() {
+        viewModel.title
+            .drive(navigationItem.rx.title)
+            .disposed(by: rx_disposeBag)
         
-    }
+        viewModel.contents
+            .bind(to: listTableView.rx.items){ tableView, row, value in
+                switch row {
+                case 0:
+                    let cell = tableView.dequeueReusableCell(withIdentifier: "contentCell")!
+                    cell.textLabel?.text = value
+                    return cell
+                case 1:
+                    let cell = tableView.dequeueReusableCell(withIdentifier: "dateCell")!
+                    cell.textLabel?.text = value
+                    return cell
+                default:
+                    fatalError()
 
+                }
+            }
+            .disposed(by: rx_disposeBag)
+    }
+    
 }
--- RxMemoTutorial/RxMemoTutorial/ViewModel/MemoDetailViewModel.swift
+++ RxMemoTutorial/RxMemoTutorial/ViewModel/MemoDetailViewModel.swift
@@ -6,7 +6,34 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
+import Action
 
-class MemoDetailViewModel {
+class MemoDetailViewModel: CommonViewModel {
+    let memo: Memo
     
+    private var formatter: DateFormatter = {
+        let f = DateFormatter()
+        f.locale = Locale(identifier: "Ko_kr")
+        f.dateStyle = .medium
+        f.timeStyle = .medium
+        return f
+    }()
+    
+    //메모 내용은 tableview에 표시됨(첫 번째 셀에는 메모 내용, 두 번째 셀에는 날짜가 표시됨)
+    //내용을 표현할 때는 observable 바인딩하고 2개이기 때문에 이렇게 문자열 배열을 방출
+    //그냥 observable로도 가능한데, why behavior subject? 메모 편집 후 다시 돌아오면 새로운 내용이 보여야 함(갱신되어야 함)
+    var contents: BehaviorSubject<[String]>
+    
+    init(memo: Memo, title: String, sceneCoordinator: SceneCoordinatorType, storage: MemoStorageType) {
+        self.memo = memo
+        
+        contents = BehaviorSubject<[String]>(value: [
+            memo.content,
+            formatter.string(from: memo.insertDate)
+        ])
+        
+        super.init(title: title, sceneCoordinator: sceneCoordinator, storage: storage)
+    }
 }
